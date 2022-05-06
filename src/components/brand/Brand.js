7600a749805761721807506da58a03973b3c6abb
Shoaib Miraki <mirakishoaib@gmail.com>
2022-05-06 21:12:39 +0430

    Brand completed

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
 // styles
-import styles from "./Brand.module.css"
+import  "./Brand.css"
+
+// Images
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
+
 
 const Brand = () => {
     return (
-        <div>
-            Brand
+        <div className='gpt3__brand section__padding'>
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={slack} alt="slack" />
+            </div>
+            <div>
+                <img src={atlassian} alt="atlassian" />
+            </div>
+            <div>
+                <img src={dropbox} alt="dropbox" />
+            </div>
+            <div>
+                <img src={shopify} alt="shopify" />
+            </div>
+
         </div>
     );
 };
