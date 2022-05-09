65657c201a4041bdc156655e92a9ec7ea50b20cf
Shoaib Miraki <mirakishoaib@gmail.com>
2022-05-09 14:21:00 +0430

    possibility completed

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 // Styles
-import styles from "./Possibility.module.css";
+import "./Possibility.css";
+
+// Image 
+import possibilityImage from "../../assets/possibility.png"
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
+        <div className='gpt3__possibility section__padding' id='possibility'>
+            <div className="gpt3__possibility-image">
+                <img src={possibilityImage} alt="possibility" />
+            </div>
+            <div className="gpt3__possibility-content">
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <h4>Request Early Access to Get Started</h4> 
+            </div>
         </div>
     );
 };
